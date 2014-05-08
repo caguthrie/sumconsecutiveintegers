acb933c9ab8fac155e0600d35118ada6aa77939e
Christopher Guthrie <christopher.a.guthrie@gmail.com>
2014-05-08 00:57:24 -0400

    Three versions

---
+++ sumintegers_v2.rb
@@ -0,0 +1,38 @@
+class SumConIntegers
+
+  attr_accessor :num  
+
+  def initialize(num)
+    @num = num
+    @hash = {}
+  end
+
+  def sum_consecutive_exists?
+    i = 1
+    while i < (@num/2) + 1
+      j = i + 1
+      sum = i
+      @hash[i] ||= []
+      return true if @hash[i].include?(sum)
+      while true
+        sum += j
+        @hash[i].push(sum)
+        return true if sum == @num
+        break if sum > @num
+        j += 1
+      end
+      i += 1
+    end
+    return false
+  end
+
+end
+t = Time.now
+s = SumConIntegers.new(2)
+i = 20000
+while i <= 20000
+  puts "#{i}: #{s.sum_consecutive_exists?}"
+  i -= 1
+  s.num = i
+end
+puts "This took #{Time.now - t} seconds"
---
+++ sumintegers_v3.rb
@@ -0,0 +1,35 @@
+class SumConIntegers
+
+  attr_accessor :num
+
+  def initialize(num)
+    @num = num
+  end
+
+  def sum_consecutive_exists?
+    i = @num/2
+    while i > 0
+      j = i + 1
+      sum = i
+      while true
+        sum += j
+        return true if sum == @num
+        break if sum > @num
+        j += 1
+      end
+      i -= 1
+    end
+    return false
+  end
+
+end
+
+t = Time.now
+s = SumConIntegers.new(2)
+i = 2
+while i < 20000
+  puts "#{i}: #{s.sum_consecutive_exists?}"
+  i += 1
+  s.num = i
+end
+puts "This took #{Time.now - t} seconds"
